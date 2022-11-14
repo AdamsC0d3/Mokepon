b4d37cf176fa68fd6cb96781ccccb8fe1a31487d
AdamsC0d3 <adamsriverog@gmail.com>
2022-11-14 11:23:48 -0500

    first commit

---
+++ mokepon.js
@@ -0,0 +1,7 @@
+alert("soy el dios de los webi wabos")
+function selecionarMascotaJugador() {
+    alert('seleccionaste mascota')
+}
+
+let botonMascotaJugador = document-getElementByid('boton-mascota')
+botonMascotaJugador.addEventListener('click, seleccionarMascotaJugador')
